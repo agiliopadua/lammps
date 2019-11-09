b9bfad2ca9e7fa8eb5f04f96ee1a451fa42e1d17
Agilio Padua <agilio.padua@gmail.com>
2019-11-09 16:01:33 +0100

    Typo in fix_conp

--- src/fix_conp.cpp
+++ src/fix_conp.cpp
@@ -66,7 +66,7 @@ FixConp::FixConp(LAMMPS *lmp, int narg, char **arg) :
 {
   if (narg < 11) error->all(FLERR,"Illegal fix conp command");
   maxiter = 100;
-  tolarence = 0.000001;
+  tolerance = 0.000001;
   everynum = force->numeric(FLERR,arg[3]);
   eta = force->numeric(FLERR,arg[4]);
   molidL = force->inumeric(FLERR,arg[5]);
@@ -901,7 +901,7 @@ void FixConp::cg()
       res[i] = res[i]-d*ap[i];
       resnorm += res[i]*res[i];
     }
-    if (resnorm/elenum_all < tolarence) {
+    if (resnorm/elenum_all < tolerance) {
       netcharge = 0.0;
       for (i = 0; i < elenum_all; ++i) netcharge += eleallq[i];
       if (me == 0) {
