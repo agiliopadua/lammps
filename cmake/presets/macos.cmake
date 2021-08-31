e2157a7d27b350919cf94948faa88847348d9483
Agilio Padua <agilio.padua@gmail.com>
2021-08-31 16:19:07 +0200

    Updated cmake presets

--- cmake/presets/macos.cmake
+++ cmake/presets/macos.cmake
@@ -2,10 +2,27 @@
 # external libraries. Compared to all_on.cmake some more unusual packages
 # are removed. The resulting binary should be able to run most inputs.
 
-set(ALL_PACKAGES PYTHON CLASS2 CORESHELL DIPOLE KSPACE MANYBODY MC MISC
-        MOLECULE QEQ RIGID VORONOI
-        USER-COLVARS USER-DRUDE USER-FEP USER-MISC USER-MOFFF USER-MOLFILE
-        USER-OMP USER-REACTION)
+set(ALL_PACKAGES
+  CLASS2
+  COLVARS
+  CORESHELL
+  DIPOLE
+  DRUDE
+  FEP
+  KSPACE
+  MANYBODY
+  MC
+  MISC
+  MOFFF
+  MOLECULE
+  MOLFILE
+  OMP
+  PYTHON
+  QEQ
+  REACTION
+  RIGID
+  USER-MISC
+  VORONOI)
 
 foreach(PKG ${ALL_PACKAGES})
   set(PKG_${PKG} ON CACHE BOOL "" FORCE)
