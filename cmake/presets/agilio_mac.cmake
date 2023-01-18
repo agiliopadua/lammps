6079bb13e76063dea174d99a1ea4f70c2c5ef5dc
agiliopadua <agilio.padua@ens-lyon.fr>
2023-01-18 13:31:07 +0100

    Added cmake preset for lmp executable

---
+++ cmake/presets/agilio_mac.cmake
@@ -0,0 +1,28 @@
+# preset that turns on a wide range of packages, some of which require
+# external libraries. Compared to all_on.cmake some more unusual packages
+# are removed. The resulting binary should be able to run most inputs.
+
+set(ALL_PACKAGES
+  COLVARS
+  CORESHELL
+  DRUDE
+  ELECTRODE
+  FEP
+  KSPACE
+  MANYBODY
+  MC
+  MISC
+  MOFFF
+  MOLECULE
+  OPENMP
+  PYTHON
+  RIGID
+  USER-MISC
+  VORONOI)
+
+foreach(PKG ${ALL_PACKAGES})
+  set(PKG_${PKG} ON CACHE BOOL "" FORCE)
+endforeach()
+
+set(BUILD_TOOLS ON CACHE BOOL "" FORCE)
+set(CMAKE_INSTALL_PREFIX "/Users/apadua" CACHE STRING "" FORCE)
